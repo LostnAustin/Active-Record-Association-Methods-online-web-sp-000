8940a045a2f5902769fa0bec33a5922434f1b1bc
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-15 19:13:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -13,7 +13,6 @@ class Song < ActiveRecord::Base
     # Hint: you won't want to create an artist record every time this method is called, only if an Drake is *not found*
     #  if nil
       song =  self.find_or_create_by(name: "Drake")
-      song.save
 
 
       # drake = drake.songs.build(:name => "Drake")
@@ -25,7 +24,6 @@ class Song < ActiveRecord::Base
       # song = Song.new(name: "Drake")
     # self.artist(name: "Drake")
      end
-     song
    end
 
 end
