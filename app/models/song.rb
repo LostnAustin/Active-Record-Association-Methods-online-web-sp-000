ac2198b74e11b22e4e87ee68bfd41dec52e32a3f
Chad Lengefeld <chadlengefeld@gmail.com>
2020-03-15 19:15:52 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,7 +15,7 @@ class Song < ActiveRecord::Base
       drake = Artist.find_or_create_by(name: "Drake")
 
       self.artist = drake
-
+      
 
       # drake = drake.songs.build(:name => "Drake")
       # drake.save
